1923e027df9b8b9cd467b512dcb9d4b01018bfd1
NagisaCo <yjr888@vip.qq.com>
2021-09-22 18:06:24 +0800

    add connection pool for mysql

--- Database/MySQL.py
+++ Database/MySQL.py
@@ -8,6 +8,58 @@ import aiomysql
 from loguru import logger
 
 
+class Connection(object):
+    def __init__(self):
+        self.conn = None
+        self.lock = asyncio.Lock()
+
+    async def connect(self, host: str, port: int, user: str, password: str, db: str):
+        self.conn = await aiomysql.connect(
+                host=host,
+                port=port,
+                user=user,
+                password=password,
+                db=db
+            )
+
+    async def disconnect(self):
+        await self.conn.ensure_closed()
+
+    async def execute(self, sql: str):
+        async with self.lock:
+            async with self.conn.cursor() as cur:
+                # logger.debug(f'Execute sql\n{sql}')
+                await cur.execute(sql)
+                last_id = cur.lastrowid
+                await self.conn.commit()
+        return last_id
+
+
+class Pool(object):
+    def __init__(self, maxsize: int = 10):
+        self.maxsize = maxsize
+        self.queue = asyncio.Queue(maxsize=self.maxsize)
+
+    async def create(self, host: str, port: int, user: str, password: str, db: str):
+        for i in range(self.maxsize):
+            connection = Connection()
+            await connection.connect(host, port, user, password, db)
+            logger.debug(f'Connection {i} created')
+            self.queue.put_nowait(connection)
+
+    async def destroy(self):
+        while not self.queue.empty():
+            conn = self.queue.get_nowait()
+            await conn.disconnect()
+
+    async def execute(self, sql: str):
+        conn = await self.queue.get()
+        result = await conn.execute(sql)
+        self.queue.put_nowait(conn)
+
+        return result
+
+
 class MySQL(object):
     def __init__(
             self,
@@ -16,7 +68,7 @@ class MySQL(object):
             user: str = "bili_live_data",
             password: str = "bili_live_data",
             db: str = "bili_live_data"):
-        self.connection = None
+        self.pool = Pool()
         self.host = host
         logger.debug(f'Get config [host]: {self.host}')
         self.port = port
@@ -30,22 +82,12 @@ class MySQL(object):
         self.lock = asyncio.Lock()
 
     async def connect(self):
-        self.connection = await aiomysql.connect(
-            host=self.host,
-            port=self.port,
-            user=self.user,
-            password=self.password,
-            db=self.db)
+        await self.pool.create(self.host, self.port, self.user, self.password, self.db)
+
         logger.info('Connected')
 
     async def disconnect(self):
-        self.connection.close()
+        await self.pool.destroy()
 
     async def execute(self, sql: str):
-        async with self.lock:
-            async with self.connection.cursor() as cur:
-                # logger.debug(f'Execute sql\n{sql}')
-                await cur.execute(sql)
-                last_id = cur.lastrowid
-                await self.connection.commit()
-        return last_id
+        return await self.pool.execute(sql)
